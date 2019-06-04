b27d1e4f1818e44824159a18b71cccf3a7c288fa
masato.achiwa <tokemdokea@yahoo.co.jp>
2019-06-04 14:43:52 +0900

    途中保存

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,24 +7,81 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+  
     @IBOutlet var tableView: UITableView!
+    
+    let realm = try! Realm()  // ←追加
+    
+    // DB内のタスクが格納されるリスト。
+    // 日付近い順\順でソート：降順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)  // ←追加
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+    }
+    
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+
+
+
+
+        // segue で画面遷移するに呼ばれる
+        func prepare(for segue: UIStoryboardSegue, sender: Any?){
+            let inputViewController:InputViewController = segue.destination as! InputViewController
+            
+            if segue.identifier == "cellSegue" {
+                let indexPath = self.tableView.indexPathForSelectedRow
+                inputViewController.task = taskArray[indexPath!.row]
+            } else {
+                let task = Task()
+                task.date = Date()
+                
+                let allTasks = realm.objects(Task.self)
+                if allTasks.count != 0 {
+                    task.id = allTasks.max(ofProperty: "id")! + 1
+                }
+                
+                inputViewController.task = task
+            }
+       
+            // 入力画面から戻ってきた時に TableView を更新させる
+            func viewWillAppear(_ animated: Bool) {
+                super.viewWillAppear(animated)
+                tableView.reloadData()
+            }
+        
+        
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     
 
-        tableView.delegate = self as? UITableViewDelegate
-        tableView.dataSource = self as? UITableViewDataSource
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
 
     // MARK: UITableViewDataSourceプロトコルのメソッド
     // データの数（＝セルの数）を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count  // ←修正する  前は　０
     }
     
     // 各セルの内容を返すメソッド
@@ -32,6 +89,20 @@ class ViewController: UIViewController {
         // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        // Cellに値を設定する.  --- ここから ---
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        // --- ここまで追加 ---
+        
+        
+    
+
         return cell
     }
     
@@ -49,6 +120,18 @@ class ViewController: UIViewController {
     
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+  
+        // --- ここから ---
+        if editingStyle == .delete {
+            // データベースから削除する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        } // --- ここまで追加 ---
+    
+    
+    
     }
 
 
